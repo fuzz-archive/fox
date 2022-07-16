f42956442c83db7b2258ac5135bf86a1de66f24d
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-07-16 16:17:32 +1000

    rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{error::Error, path::Path, fs::{read, write}};
-use aes_gcm::{Aes256Gcm, Key, Nonce};
 use aes_gcm::aead::{Aead, NewAead};
+use aes_gcm::{Aes256Gcm, Key, Nonce};
 use clap::Parser;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+use std::{
+    error::Error,
+    fs::{read, write},
+    path::Path,
+};
 
 const NONCE: &[u8] = b"Random Nonce";
 
@@ -11,54 +17,59 @@ const NONCE: &[u8] = b"Random Nonce";
 #[clap(author = "Artie")]
 #[clap(about = "A simple file Encryption CLI")]
 struct CLI {
-  #[clap(short, long)]
-  key: String,
-  file: String
+    #[clap(short, long)]
+    key: String,
+    file: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  let arg = CLI::parse();
-  let key: String = arg.key;
-  let file: String = arg.file;
-  let path_file = Path::new(&file);
+    let arg = CLI::parse();
+    let key: String = arg.key;
+    let file: String = arg.file;
+    let path_file = Path::new(&file);
 
-  if key.is_empty() {
-    println!("You must provide a key.");
+    let file_content = read(path_file)?;
 
-    return Ok(());
-  }
+    if path_file.extension().unwrap() == "fox" {
+        if key.is_empty() {
+            println!("You must provide a key to decrypt the file.");
 
-  if key.len() < 32 {
-    println!("Your key must be 32 characters long.");
+            return Ok(());
+        }
 
-    return Ok(());
-  }
+        let cipher_key = Key::from_slice(&key.as_bytes());
+        let nonce = Nonce::from_slice(NONCE);
+        let cipher = Aes256Gcm::new(cipher_key);
 
-  if key.len() > 32 {
-    println!("Your key must be 32 characters long.");
+        let decrypted_content = cipher
+            .decrypt(nonce, file_content.as_ref())
+            .expect("Could not decrypt file");
 
-    return Ok(());
-  }
+        write(
+            format!("{}", path_file.file_stem().unwrap().to_str().unwrap()),
+            decrypted_content,
+        )?;
 
-  let file_content = read(path_file)?;
-  let cipher_key = Key::from_slice(&key.as_bytes());
-  let nonce = Nonce::from_slice(NONCE);
-  let cipher = Aes256Gcm::new(cipher_key);
+        return Ok(());
+    }
 
-  if path_file.extension().unwrap() == "fox" {
-    let decrypted_content = cipher.decrypt(nonce, file_content.as_ref())
-      .expect("Could not decrypt file");
+    let mut rnd = rand::thread_rng();
+    let rnd_key: String = (0..32).map(|_| rnd.sample(Alphanumeric) as char).collect();
+    let cipher_key = Key::from_slice(&rnd_key.as_bytes());
+    let nonce = Nonce::from_slice(NONCE);
+    let cipher = Aes256Gcm::new(cipher_key);
 
-    write(format!("{}", path_file.file_stem().unwrap().to_str().unwrap()), decrypted_content)?;
+    let encrypted_content = cipher
+        .encrypt(nonce, file_content.as_ref())
+        .expect("Could not encrypt file...");
 
-    return Ok(());
-  }
+    write(
+        format!("{}.fox", path_file.file_name().unwrap().to_str().unwrap()),
+        encrypted_content,
+    )?;
 
-  let encrypted_content = cipher.encrypt(nonce, file_content.as_ref())
-    .expect("Could not encrypt file...");
+    println!("Decryption Key: {}", rnd_key);
 
-  write(format!("{}.fox", path_file.file_name().unwrap().to_str().unwrap()), encrypted_content)?;
-  
-  Ok(())
+    Ok(())
 }
