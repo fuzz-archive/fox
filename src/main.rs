64f21a82b1c96ccd880f0048831c1dcb51a80894
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-07-16 15:44:02 +1000

    Refactor all that stuff

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ const NONCE: &[u8] = b"Random Nonce";
 
 #[derive(Parser)]
 #[clap(name = "Fox ^^")]
+#[clap(version = env!("CARGO_PKG_VERSION"))]
+#[clap(author = "Artie")]
+#[clap(about = "A simple file Encryption CLI")]
 struct CLI {
   #[clap(short, long)]
   key: String,
-  #[clap(short, long, default_value = "./out")]
-  out: String,
   file: String
 }
 
@@ -23,7 +24,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
   let path_file = Path::new(&file);
 
   if key.is_empty() {
-    println!("You must provide a key");
+    println!("You must provide a key.");
+
+    return Ok(());
+  }
+
+  if key.len() < 32 {
+    println!("Your key must be 32 characters long.");
+
+    return Ok(());
+  }
+
+  if key.len() > 32 {
+    println!("Your key must be 32 characters long.");
 
     return Ok(());
   }
@@ -37,7 +50,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let decrypted_content = cipher.decrypt(nonce, file_content.as_ref())
       .expect("Could not decrypt file");
 
-    write(format!("{}.fox", arg.out), decrypted_content)?;
+    write(format!("{}", path_file.file_stem().unwrap().to_str().unwrap()), decrypted_content)?;
 
     return Ok(());
   }
@@ -45,7 +58,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
   let encrypted_content = cipher.encrypt(nonce, file_content.as_ref())
     .expect("Could not encrypt file...");
 
-  write(format!("{}.fox", arg.out), encrypted_content)?;
+  write(format!("{}.fox", path_file.file_name().unwrap().to_str().unwrap()), encrypted_content)?;
   
   Ok(())
 }
